dc83d6be25770f72f9db41d4fe601f1f43915122
m305-18\w401 <cheung00528@gmail.com>
2018-04-12 17:58:51 +0800

    作業完成

---
+++ text01/text01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+           
+            List<int> numbers = new List<int>();
+            for (int i=1;i<=42;i++) 
+            {
+                numbers.Add(i);
+            }
+            for (int a = 0; a < 6; a++)
+            {
+                 int r = rand.Next(1, 43);
+                 Console.WriteLine(numbers [r]+"");
+                numbers.RemoveAt(r);
+            }
+           
+           
+            Console.ReadKey();
+        }
+    }
+}
